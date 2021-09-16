f5eac5689ebaa5fb2caadf3f6cf501a148c91b1c
rafi021 <mahmud.ibrahim021@gmail.com>
2021-09-16 22:58:06 +0600

    routes comment

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Frontend/Public Panel Routes Start
 Route::get('/', function () {
     return view('welcome');
 });
+// Frontend/Public Panel Routes End
 
+
+
+// Admin Panel Routes Start
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+// Admin Panel Routes End
