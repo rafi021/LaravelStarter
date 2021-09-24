1cc0cea01a05ddafe2a225548c20f263d1e2ea9e
rafi021 <mahmud.ibrahim021@gmail.com>
2021-09-24 21:41:06 +0600

    Seeting Basic Setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             PageSeeder::class,
             MenuSeeder::class,
+            SettingSeeder::class,
         ]);
     }
 }
